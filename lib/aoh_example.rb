cd673aee3efe182bd9742f8f78c3107a5c8b2f0d
Scott Singer <ssinger303@gmail.com>
2020-05-03 01:43:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,8 +18,8 @@ def literal_aoh
   array_literal=[ { :name =>"Don Gately", :occupation=> "Live-in Staff" },
   { :name=>"Joelle van Dyne", :occupation=>"Radio Personality" },
   { :name=>"Pat Monteseian", :occupation=>"Staff" },
-  { :name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  { :name=>"Kate Gompert", :occupation=>"None" },
+  { :name=>"Bruce Green", :occupation=>"Fan of Mildred" }]
 
 end
 
